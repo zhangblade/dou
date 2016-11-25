36255c31ea1c668f26e26933789160f66b6ef40e
峰 <851715387@qq.com>
2016-11-25 19:26:32 +0800

    Update search.js

--- directive/search.js
+++ directive/search.js
@@ -1,4 +1,8 @@
 (function(){var a=angular.module("douban.SearchModule",["douban.listModule"]);
             a.directive("search",["$routeParams","$location","$route","$timeout","$rootScope",
                                   function(d,f,e,c,b){
-              return{template:'<div id="navbar" class="navbar-collapse collapse col-xs-4" style="display:inline-block;float:right">                             <form ng-submit="search()" class="navbar-form navbar-right">                            <input ng-model="input" type="text" class="form-control" placeholder="Search...">                           </form>                         </div>',replace:true,link:function(h,j,i,g){h.search=function(){if(d.category){e.updateParams({category:"search",q:b.input})}else{f.path("search");c(function(){e.updateParams({category:"search",q:b.input})},0)}}}}}])})();
+              return{template:'<div id="navbar" class="navbar-collapse collapse">     
+                     <form ng-submit="search()" class="navbar-form navbar-right" style="display:inline-block;">     
+                     <input ng-model="input" type="text" class="form-control" placeholder="Search...">    
+                     </form>                     
+                     </div>',replace:true,link:function(h,j,i,g){h.search=function(){if(d.category){e.updateParams({category:"search",q:b.input})}else{f.path("search");c(function(){e.updateParams({category:"search",q:b.input})},0)}}}}}])})();
